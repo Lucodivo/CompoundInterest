54cf3301c7a48b84aa5e9d4bc6ac885fb38ccfa8
Lucodivo <chaskins483@student.msjc.edu>
2015-02-18 18:41:25 -0800

    Added more comments. Removed useless variables.

--- Main.cpp
+++ Main.cpp
@@ -1,19 +1,21 @@
-//Here is a simple C++ program I just created for compound interest 
+//A simple C++ program for compound interest 
 //that might also use monthly compounding
-//Not sure if it's what you were looking for but here it is! :)
 
 #include <iostream>
 
 double compoundInterest(double initialValue, float percentRate, int numYears, bool isCompMonthly);
 
 int main(){
+	//interest variables
 	double initialInvestment;
 	float investmentRate;
 	int yearsToInvest;
 	char isCompoundMonthly;
 
+	//end investment after calculation
 	double endInvestment;
 
+	//acquiring input from user
 	std::cout << "input your initial investment" << std::endl;
 	std::cin >> initialInvestment;
 	std::cout << "input the investment rate[ex: input \"2\" for a 2% rate]" << std::endl;
@@ -22,6 +24,8 @@ int main(){
 	std::cin >> yearsToInvest;
 	std::cout << "input \"y\" for monthly compound or \"n\" for simple annual compound" << std::endl;
 	std::cin >> isCompoundMonthly;
+
+	//calculation changes on whether it should be compounded monthly or not	
 	if (isCompoundMonthly == 'y' || isCompoundMonthly == 'Y'){
 		endInvestment = compoundInterest(initialInvestment, investmentRate, yearsToInvest, true);
 	}
@@ -29,11 +33,14 @@ int main(){
 		endInvestment = compoundInterest(initialInvestment, investmentRate, yearsToInvest, false);
 	}
 
-	int quickTruncation = (int)(endInvestment /= .01);
-	endInvestment = quickTruncation * .01;
+	//quick truncation to display only 2 decimal places
+	endInvestment = (int)(endInvestment /= .01) * .01;
+	
+	//inform user of the end investment
 	std::cout << "Your end investment will be: $" << endInvestment << std::endl;
 }
 
+//recursive function to calculate compound interest
 double compoundInterest(double initialValue, float percentRate, int numYears, bool isCompMonthly){
 	if (isCompMonthly){
 		numYears *= 12;
